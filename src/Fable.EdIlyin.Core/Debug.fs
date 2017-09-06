e375d74fc69693c3e66d234b7c0169dad8e87376
Ed Ilyin <ed.il@live.com>
2017-09-06 18:44:57 +0300

    clean

--- src/Fable.EdIlyin.Core/Debug.fs
+++
@@ -1,6 +0,0 @@
-module Fable.EdIlyin.Core.Debug
-
-
-let log tag x =
-    do printfn "%s: %A" tag x
-    x
--- src/Tests/Main.fs
+++
@@ -1,9 +0,0 @@
-module Tests
-
-
-open Fable.Core.JsInterop
-
-// This is necessary to make webpack collect all test files
-importAll "./FetchTests.fs"
-importAll "./ResultTests.fs"
-importAll "./ThrottleTests.fs"
