dbe635080845df32b15561e983b65102318efff8
Ed Ilyin <ed.il@live.com>
2017-09-08 19:24:51 +0300

    0.0.23

--- src/Fable.EdIlyin.Core/Json/Encode.fs
+++ src/Fable.EdIlyin.Core/Json/Encode.fs
@@ -9,10 +9,22 @@ let object fields = createObj fields
 let string (x: string) = box x
 
 
+let int16 (x: int16) = box x
+
+
 let int (x: int) = box x
 
 
 let int64 (x: int64) = box x
+
+
+let uint16 (x: uint16) = box x
+
+
+let uint32 (x: uint32) = box x
+
+
+let uint64 (x: uint64) = box x
 
 
 let float (x: float) = box x
