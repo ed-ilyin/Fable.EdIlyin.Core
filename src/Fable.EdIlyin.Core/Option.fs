d144b7855d2f184b3c4c78d5586d62df3e838047
Ed Ilyin <ed.il@live.com>
2017-09-11 16:39:07 +0300

    0.0.29 adds Option.andMap, map2

--- src/Fable.EdIlyin.Core/Option.fs
+++ src/Fable.EdIlyin.Core/Option.fs
@@ -2,3 +2,17 @@ module Fable.EdIlyin.Core.Option
 
 
 let withDefault def = function None -> def | Some value -> value
+
+
+let andMap option functionOption =
+    match functionOption, option with
+        | Some func, Some value -> func value |> Some
+        | _ -> None
+
+
+let (<*>) functionOption option = andMap option functionOption
+
+
+//val map2 : mapping:('T1 -> 'T2 -> 'U) -> 'T1 option -> 'T2 option -> 'U option
+let map2 mapping option1 option2 =
+    Some mapping <*> option1 <*> option2
